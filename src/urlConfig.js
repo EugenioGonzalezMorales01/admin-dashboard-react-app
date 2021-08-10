1cfcb94bbf2478c150eb537aad59a8e9c3a1e514
EugenioGonzalezMorales01 <88595236+EugenioGonzalezMorales01@users.noreply.github.com>
2021-08-10 01:40:59 -0500

    routes updates (localhost deleted)

--- src/urlConfig.js
+++ src/urlConfig.js
@@ -1,4 +1,4 @@
-export const api = "http://localhost:2000/api";
+export const api = "https://dashboard-rest.herokuapp.com/api";
 export const generatePublicUrl = (fileName) => {
-	return `http://localhost:2000/public/${fileName}`;
+	return `https://dashboard-rest.herokuapp.com/public/${fileName}`;
 };
